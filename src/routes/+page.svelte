41c81f6a7f3082c44259572d338551fd91eff511
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-03-31 13:00:22 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card'
+</script>
+
+<Card.Root class="w-96 mx-auto my-4">
+	<Card.Header>
+		<Card.Title>
+			I am the title
+		</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		I am the content
+	</Card.Content>
+</Card.Root>
